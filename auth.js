91c355c25e5a0a8ee405b86affcd618d3b48b54a
eman-soomro <eman.67063@iqra.edu.pk>
2026-06-13 22:41:02 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://ewastpsqndqjtiuaagwy.supabase.co";
-const SUPABASE_KEY = "ewastpsqndqjtiuaagwy";
+const SUPABASE_KEY = "sb_secret_3TdYy07EHugZv05JkjDvdQ_o4FpgMiT";
 
 // IMPORTANT: only ONE client, never redeclare supabase
 const client = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
